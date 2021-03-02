966f6228338e28d57634b401edb4b85fc4cc0513
Dominik Honzak <465551@mcpsmd.net>
2021-03-02 14:40:03 +0000

    Update 1.0.0

--- main.r
+++ main.r
@@ -18,7 +18,7 @@ barplot(y, names.arg = x,col = c("red","green","blue","purple"),main = "Unknown 
 line1 <- c(1,2,3,4,5,10)
 line2 <- c(2,5,7,8,9,10)
 
-plot(line1, type = "l", col = "blue")
+plot(line1, type = "l", col = "blue",main = "Random Graph")
 lines(line2, type="l", col = "red")
 
 
@@ -33,7 +33,7 @@ y1 <- c(99,86,87,88,111,103,87,94,78,77,85,86)
 x2 <- c(2,2,8,1,15,8,12,9,7,3,11,4,7,14,12)
 y2 <- c(100,105,84,105,90,99,90,95,94,100,79,112,91,80,85)
 
-plot(x1, y1, main="Observation of Cars", xlab="Car age", ylab="Car speed", col="red", cex=2)
+plot(x1, y1, main="More Random", xlab="Random1", ylab="Random2", col="red", cex=2)
 points(x2, y2, col="blue", cex=2)
 
 
@@ -45,10 +45,10 @@ points(x2, y2, col="blue", cex=2)
 x <- c(10,20,30,40)
 
 # Create a vector of labels
-mylabel <- c("Apples", "Bananas", "Cherries", "Dates")
+mylabel <- c("Apples", "Bananas", "More Apples", "Oranges")
 
 # Create a vector of colors
-colors <- c("blue", "yellow", "green", "black")
+colors <- c("red", "yellow", "red4", "orange")
 
 # Display the pie chart
 pie(x, label = mylabel, main = "Pie Chart", col = colors)
